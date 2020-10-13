f07434e0cd885cb30d754b6c9916864709175503
HDKHALILI <14176079+HDKHALILI@users.noreply.github.com>
2020-10-14 09:50:59 +1100

    modify cat-class.js

--- cat-class.js
+++ cat-class.js
@@ -1,12 +1,24 @@
 class Cat {
+  static genericGreeting() {
+    console.log("Hello! I'm a cat!");
+  }
+
   constructor(name) {
     this.name = name;
   }
 
-  greet() {
+  personalGreeting() {
     console.log(`Hello! My name is ${this.name}`);
+  }
+
+  rename(newName) {
+    this.name = newName;
   }
 }
 
 let kitty = new Cat("Sophie");
-kitty.greet();
+kitty.personalGreeting();
+console.log(kitty.name); // Sophie
+kitty.rename("Chloe");
+console.log(kitty.name); // Chloe
+Cat.genericGreeting();
